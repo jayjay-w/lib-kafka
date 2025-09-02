548747bb29eb0d36ef663793232a374d581e5ca8
Jay Joshua <joejoshw@gmail.com>
2025-09-02 10:57:49 +0200

    Fix gem structure

--- lib/lib-kafka.rb
+++ lib/lib-kafka.rb
@@ -1,48 +1,19 @@
 # frozen_string_literal: true
 
-require "rdkafka"
-require "json"
-require "singleton" # <-- This line is new
-require "securerandom"
+require_relative "lib-kafka/version"
+require_relative "lib-kafka/kafka_producer"
 
 module LibKafka
-  # A shared Kafka producer class that uses the singleton pattern.
-  # This ensures there is only one instance of the producer across the application.
-  class KafkaProducer
-    include Singleton # <-- This line is new
+  class << self
+    attr_accessor :config
+  end
 
-    def initialize
-      # The configuration is now handled by the gem's initializer
-      # in the host application.
-      puts "Initializing KafkaProducer with brokers: #{LibKafka.config.kafka_servers}"
-      config = {
-        "bootstrap.servers": LibKafka.config.kafka_servers
-      }
-      @producer = Rdkafka::Config.new(config).producer
-    end
+  def self.configure
+    self.config ||= Config.new
+    yield(config)
+  end
 
-    # Method to publish a message with metadata to a specific topic.
-    def produce(topic, message)
-      # Get the hostname of the machine running the service.
-      hostname = `hostname`.strip
-
-      # Add metadata to the message payload, using the configured service name.
-      payload_with_metadata = {
-        data: message,
-        metadata: {
-          sent_from: LibKafka.config.service_name,
-          timestamp: Time.now.utc.iso8601,
-          hostname: hostname,
-          correlation_id: SecureRandom.uuid
-        }
-      }
-
-      # Convert the entire payload to JSON.
-      payload = payload_with_metadata.to_json
-
-      @producer.produce(topic: topic, payload: payload, partition: -1)
-      @producer.flush(10) # Ensure message is sent
-      puts "Published message to '#{topic}': #{payload}"
-    end
+  class Config
+    attr_accessor :kafka_servers, :service_name
   end
 end
